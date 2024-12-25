7fd139a859ab9fa1c382e73d298fdbb15de2efd7
cansu <cansualtunsoy@gmail.com>
2024-12-25 14:29:23 +0300

    work

---
+++ src/c02/C10_DoWhileLoop_Karsilastirma.java
@@ -0,0 +1,38 @@
+package c02;
+
+public class C10_DoWhileLoop_Karsilastirma {
+
+    public static void main(String[] args) {
+
+
+        /*
+        - While loop once sarti kontrol edip, sarti sagliyorsak loop body calisir
+        - do-while loop ise once body'yi calistirir, sonra kontrol yapar
+
+
+        - Do-while loop'un artisida eksiside budur, EGER en az bir kere loop calismasini isterseniz avantaj olur
+
+        EGER sarti saglamiyorsa HIC CALISMASIN derseniz dezavantaj olur
+         */
+
+
+        int sayi = -20;
+        // sayi 0'dan buyuk ise, 0 oluncaya kadar birer azaltip yazdirin
+
+        while (sayi > 0) {
+            System.out.println(sayi + " ");
+            sayi--;
+        }
+
+
+        // ===========================================================================================
+
+
+        do {
+            System.out.println(sayi + " ");
+            sayi--;
+
+        } while (sayi > 0);
+
+    }
+}
