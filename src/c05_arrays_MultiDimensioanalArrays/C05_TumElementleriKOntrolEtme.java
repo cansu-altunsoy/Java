66a1cd2acb2a946076e3c6541dbf05922957f09a
cansu <cansualtunsoy@gmail.com>
2025-01-01 13:29:31 +0300

    arrayv2

--- src/c05_arrays_MultiDimensioanalArrays/C05_TumElementleriKOntrolEtme.java
+++ src/c05_arrays_MultiDimensioanalArrays/C05_TumElementleriKOntrolEtme.java
@@ -18,7 +18,6 @@ public class C05_TumElementleriKOntrolEtme {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
 
-
                 if (arr[i][j] % 2 == 0) {
 
                     toplam += arr[i][j];
